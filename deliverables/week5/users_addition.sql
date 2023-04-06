5c6db9e994f19eb208bfacd3d7f048c37b81faa8
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2023-04-06 16:10:02 -0400

    adding database creation and user scripts

---
+++ deliverables/week5/users_addition.sql
@@ -0,0 +1,10 @@
+CREATE USER [krzyzehj] FROM LOGIN [krzyzehj]
+EXEC sp_addrolemember'db_owner', 'krzyzehj';
+
+GO
+
+
+CREATE USER [oaldonda] FROM LOGIN [oaldonda]
+EXEC sp_addrolemember'db_owner', 'oaldonda';
+
+GO
